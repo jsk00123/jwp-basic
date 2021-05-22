360e662094ce5b8e0708cfd4449dcd3c3c737ea5
jsk00123 <jsk0012345@gmail.com>
2021-05-22 17:03:50 +0900

    [study-0522] mvc implements

--- src/main/java/next/controller/UpdateUserController.java
+++ src/main/java/next/controller/UpdateUserController.java
@@ -16,33 +16,31 @@ import core.db.DataBase;
 import next.model.User;
 
 @WebServlet(value = { "/users/update", "/users/updateForm" })
-public class UpdateUserController extends HttpServlet {
+public class UpdateUserController implements Controller {
     private static final long serialVersionUID = 1L;
     private static final Logger log = LoggerFactory.getLogger(UpdateUserController.class);
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String userId = req.getParameter("userId");
-        User user = DataBase.findUserById(userId);
-        if (!UserSessionUtils.isSameUser(req.getSession(), user)) {
-            throw new IllegalStateException("다른 사용자의 정보를 수정할 수 없습니다.");
-        }
-        req.setAttribute("user", user);
-        RequestDispatcher rd = req.getRequestDispatcher("/user/updateForm.jsp");
-        rd.forward(req, resp);
-    }
+    public String execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+        if("POST".equals(req.getMethod())) {
+            User user = DataBase.findUserById(req.getParameter("userId"));
+            if (!UserSessionUtils.isSameUser(req.getSession(), user)) {
+                throw new IllegalStateException("다른 사용자의 정보를 수정할 수 없습니다.");
+            }
 
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        User user = DataBase.findUserById(req.getParameter("userId"));
-        if (!UserSessionUtils.isSameUser(req.getSession(), user)) {
-            throw new IllegalStateException("다른 사용자의 정보를 수정할 수 없습니다.");
+            User updateUser = new User(req.getParameter("userId"), req.getParameter("password"), req.getParameter("name"),
+                    req.getParameter("email"));
+            log.debug("Update User : {}", updateUser);
+            user.update(updateUser);
+            return "redirect:/";
+        } else {
+            String userId = req.getParameter("userId");
+            User user = DataBase.findUserById(userId);
+            if (!UserSessionUtils.isSameUser(req.getSession(), user)) {
+                throw new IllegalStateException("다른 사용자의 정보를 수정할 수 없습니다.");
+            }
+            req.setAttribute("user", user);
+            return "/user/updateForm.jsp";
         }
-
-        User updateUser = new User(req.getParameter("userId"), req.getParameter("password"), req.getParameter("name"),
-                req.getParameter("email"));
-        log.debug("Update User : {}", updateUser);
-        user.update(updateUser);
-        resp.sendRedirect("/");
     }
 }
